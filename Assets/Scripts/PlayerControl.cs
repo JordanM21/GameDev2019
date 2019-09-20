987a3e96da1306f7ee3f6575c7d98e777165e394
Jordan <jordan.z616@stu.nebo.edu>
2019-09-20 10:31:53 -0600

    Started on the challenge and got it to move

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,23 +4,19 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //Player control private variables
+    private float speed = 5.0f;
+    private float turnSpeed = 5f;
+    private float horizontalInput;
+    private float forwardInput;
+  
 
     // Update is called once per frame
     void Update()
     {
+        //This code moves the vehicle forward and back, left and right.
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-
-        //We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
