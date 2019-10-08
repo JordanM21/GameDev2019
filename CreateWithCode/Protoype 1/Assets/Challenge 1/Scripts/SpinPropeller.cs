d1e2b924899b72863b8fb55a3af34f8acb9cc60e
Jordan <jordan.z616@stu.nebo.edu>
2019-10-08 10:29:42 -0600

    Created Protoype2
    Player can now move and has boundaries

---
+++ CreateWithCode/Protoype 1/Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropeller : MonoBehaviour
+{
+    private float rotationSpeed = 200f;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotationSpeed);
+
+    }
+}
---
+++ CreateWithCode/Protoype 1/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    //Player control private variables
+    private float speed = 5.0f;
+    private float turnSpeed = 5f;
+    private float horizontalInput;
+    private float forwardInput;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This code moves the vehicle forward and back, left and right.
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
