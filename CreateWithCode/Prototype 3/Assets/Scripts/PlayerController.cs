54ca7ab13ddec68cdba285a1024eebafc4dc19de
Jordan <jordan.z616@stu.nebo.edu>
2019-11-07 10:32:28 -0700

    Finished Prototype 3
    Finalized prototype 3

--- CreateWithCode/Prototype 3/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float jumpForce;
+    public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
 
@@ -21,10 +21,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
         }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
