e00e2af07b0a460a262e7abf495a7904c0d4fb84
Jordan <jordan.z616@stu.nebo.edu>
2019-11-19 10:28:50 -0700

    Added particles
    Added explosion and dirt particles

--- CreateWithCode/Prototype 3/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -6,27 +6,30 @@ public class SpawnManager : MonoBehaviour
 {
 
     public GameObject obstaclePrefab;
-    private Vector3 spawnPos = new Vector3(32, 0, 0);
+    private Vector3 spawnPos = new Vector3(37, 0, 0);
     private float startDelay = 2;
-    private float repeatDelay = 2;
+    private float repeatRate = 2;
     private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", startDelay, repeatDelay);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
         playerControllerScript =
-            GameObject.Find("Player").GetComponent<PlayerController>();
+        GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     void SpawnObstacle()
     {
-        if(playerControllerScript.gameOver == false)
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
     }
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
