2a480d01382cb57239042bce8b9090b5dc65c0a6
Jordan <jordan.z616@stu.nebo.edu>
2019-12-04 10:28:54 -0700

    Finished Lab 3
    Lab 3 done

--- CreateWithCode/Lab2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Lab2/Assets/Scripts/PlayerController.cs
@@ -9,16 +9,31 @@ public class PlayerController : MonoBehaviour
     private Rigidbody PlayerRb;
     private float speed = 10.0f;
     public float horizontalInput;
-   
+    public float xRange = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //adds rigidbody to our player
+        PlayerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Makes our player move left to right and makes it so it can't move off the screen
+        if (transform.position.x < -7)
+        {
+            transform.position = new Vector3(-7, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > 18)
+        {
+            transform.position = new Vector3(18, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
     }
+    
 }
