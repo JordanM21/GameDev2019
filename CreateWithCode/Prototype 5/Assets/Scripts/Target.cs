e8ed534c3933566f78f6dbb12590f45ca8c2c738
Jordan <jordan.z616@stu.nebo.edu>
2019-12-18 10:30:09 -0700

    Finished 4.1 and 4.2
    Finished unit 4.1 and 4.2

--- CreateWithCode/Prototype 5/Assets/Scripts/Target.cs
+++ CreateWithCode/Prototype 5/Assets/Scripts/Target.cs
@@ -11,6 +11,9 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -18,8 +21,30 @@ public class Target : MonoBehaviour
         targetRb = GetComponent<Rigidbody>();
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
-        transform.position = new Vector3(RandomSpawnPos;
+        transform.position = RandomSpawnPos();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
+
+    private void OnMouseDown()
+    {
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            gameManager.UpdateScore(pointValue);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+
+   
 
 
     Vector3 RandomForce() { return Vector3.up * Random.Range(minSpeed, maxSpeed);  }
